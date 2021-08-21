a5769ed7b02cdea6f78faec51969bffb04677706
20SH367 <72449254+20SH367@users.noreply.github.com>
2021-08-21 18:15:46 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -19,11 +19,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -32,44 +27,28 @@ def upload_form():
 
 
 @app.route('/', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File successfully uploaded')
-            return redirect('/')
-        else:
-            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-            return redirect(request.url)
-@app.route('/', methods=['POST'])
-def upload_video():
+def upload_work():
 	if 'file' not in request.files:
 		flash('No file part')
 		return redirect(request.url)
 	file = request.files['file']
 	if file.filename == '':
-		flash('No image selected for uploading')
+		flash('PLEASE SELECT YOUR WORK')
 		return redirect(request.url)
 	else:
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 		#print('upload_video filename: ' + filename)
-		flash('Video successfully uploaded and displayed below')
+		flash('SUCESSFULLY UPLOADED')
 		return render_template('upload.html', filename=filename)
 
 @app.route('/display/<filename>')
-def display_video(filename):
-	#print('display_video filename: ' + filename)
+def display_work(filename):
+	#print('display_work filename: ' + filename)
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+
 
 
 if __name__ == "__main__":
