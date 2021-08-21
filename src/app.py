c15713b469d1f0453034ee291a0239d6899d75ca
20SH367 <72449254+20SH367@users.noreply.github.com>
2021-08-21 18:04:04 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -50,6 +50,26 @@ def upload_file():
         else:
             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
             return redirect(request.url)
+@app.route('/', methods=['POST'])
+def upload_video():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	else:
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_video filename: ' + filename)
+		flash('Video successfully uploaded and displayed below')
+		return render_template('upload.html', filename=filename)
+
+@app.route('/display/<filename>')
+def display_video(filename):
+	#print('display_video filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 if __name__ == "__main__":
